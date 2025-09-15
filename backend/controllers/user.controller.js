53ddc03f88dbda5abb385e46493020e44b20e760
Segasonics <114384404+Segasonics@users.noreply.github.com>
2025-09-15 20:55:38 +0530

    removed maxage in tokenoptions

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -8,14 +8,12 @@ const accessTokenOptions = {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    maxAge: 15 * 60 * 1000 // 15 minutes
 };
 
 const refreshTokenOptions = {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
 };
 
 
